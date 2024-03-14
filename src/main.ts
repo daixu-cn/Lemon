f97587a86b0e57dc713ba43d034cecd7558ef538
daixu <daixu.cn@outlook.com>
2024-03-14 09:45:42 +0800

    feat(custom): 引入duration插件

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import { createPinia } from "pinia"
 import dayjs from "dayjs"
 import "dayjs/locale/zh-cn"
 import localizedFormat from "dayjs/plugin/localizedFormat"
+import duration from "dayjs/plugin/duration"
 
 dayjs.locale("zh-cn")
 dayjs.extend(localizedFormat)
+dayjs.extend(duration)
 
 const app = createApp(App)
 app.use(router).use(createPinia()).mount("#app")
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs"
+
 /**
  * @description 获取两个数之间的随机整数（包含最大值和最小值）
  * @param {number} min 最小值
@@ -8,4 +10,23 @@ export const getRandomIntInclusive = (min: number, max: number): number => {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+/**
+ * @description 计算间隔时间
+ * @param {dayjs.ConfigType} date 日期
+ * @return {string} 间隔时间结果
+ */
+export function calculateAge(date: dayjs.ConfigType): string {
+  const duration = dayjs.duration(dayjs().diff(date, "day"), "days")
+
+  const years = duration.years()
+  const months = duration.months()
+  const remainingDays = duration.days()
+
+  if (years === 0 && months === 0) return `${remainingDays}天`
+  if (years === 0) return `${months}月${remainingDays}天`
+  if (months === 0) return `${years}岁${remainingDays}天`
+
+  return `${years}岁${months}月${remainingDays}天`
 }
